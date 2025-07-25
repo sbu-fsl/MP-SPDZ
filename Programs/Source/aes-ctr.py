61c1f61c3b3cfd88ffd0ed486001bb8e67b471ad
Christopher Smith <csasmith@cs.stonybrook.edu>
2025-07-25 15:57:42 -0700

    aes-ctr passes correctness test

--- Programs/Source/aes-ctr.py
+++ Programs/Source/aes-ctr.py
@@ -2,53 +2,71 @@ from aes import AESCipher, apply_field_embedding, apply_inverse_field_embedding,
 from Compiler.library import print_ln
 from Compiler.types import cgf2n, sgf2n, regint
 from Compiler.compilerLib import Compiler
-from Compiler.GC.types import sbits # requires binary circuits. Have to compile with -X or -Y option for daBit / edaBit
 
 MAX_BLOCKS = 2 ** 32  # maximum number of plaintext blocks we can handle in AES-CTR mode
 
-class AES_CTR():
-    def __init__(self, key: list[sgf2n], num_blocks: int):
-        '''
-        Initialize nonce and counters for AES-CTR mode for num_blocks of plaintext.
-        1 block = 16 bytes = 128 bits.
 
-        :param key: 16-byte key for AES (unembedded)
-        :param num_blocks: number of blocks of plaintext to encrypt/decrypt
-        '''
-        num_rounds_from_key_length = {16: 10, 24: 12, 32: 14}
-        assert(len(key) in num_rounds_from_key_length)  # key must be 16, 24, or 32 bytes long
-        self.aes = AESCipher(num_rounds_from_key_length[len(key)], key)
+def aes_ctr_encrypt(key: list[sgf2n], plaintext: list[sgf2n], nonce: list[cgf2n] = None) -> tuple[list[sgf2n], list[sgf2n]]:
+    '''
+    Encrypt plaintext using AES-CTR mode.
 
-        assert(num_blocks <= MAX_BLOCKS)
-        self.num_blocks = num_blocks
-        # nonce should be 96 bits = 12 bytes, and counter should be 32 bits = 4 bytes.
-        self.nonce = [cgf2n(regint.get_random(bit_length=8)) for _ in range(12)]
-        # TODO: for parallelism, maybe we can wrap counters in a single cgf2n?
-        self.counters = [self.nonce + [cgf2n(x) for x in i.to_bytes(length=4)] for i in range(num_blocks)]
+    :param plaintext: plaintext to encrypt. Length of plaintext should be multiple of BLOCK_SIZE * BYTES_PER_WORD, since we don't yet support padding.
+    :return: A tuple with the nonce as the first coordinate, and the ciphertext as the second coordinate. 
+    '''
+    # validate plaintext length and set up nonce + counters based on length of plaintext
+    assert(len(plaintext) % (BLOCK_SIZE * BYTES_PER_WORD) == 0) # Too lazy to deal with padding.
+    num_blocks = int(len(plaintext) / (BLOCK_SIZE * BYTES_PER_WORD))
+    assert(num_blocks <= MAX_BLOCKS)
+    if(nonce is None):
+        nonce = [cgf2n(regint.get_random(bit_length=8)) for _ in range(12)]
+    counters = [[cgf2n(x) for x in i.to_bytes(length=4)] for i in range(num_blocks)]
 
-    def encrypt(self, plaintext: list[sgf2n]) -> tuple[list[sgf2n], list[sgf2n]]:
-        '''
-        Encrypt plaintext using AES-CTR mode.
+    # AES block cipher setup (performs key expansion)
+    num_rounds_from_key_length = {16: 10, 24: 12, 32: 14}
+    assert(len(key) in num_rounds_from_key_length)  # key must be 16, 24, or 32 bytes long
+    aes = AESCipher(num_rounds_from_key_length[len(key)], key)
 
-        :param plaintext: plaintext to encrypt. Length of plaintext should be self.num_blocks * BLOCK_SIZE * BYTES_PER_WORD, since we don't yet support padding.
-        :return: list of sgf2n values representing the ciphertext
-        '''
-        assert(len(plaintext) == self.num_blocks * BLOCK_SIZE * BYTES_PER_WORD) # Too lazy to deal with padding.
-        # chunk plaintext into blocks and copy to ciphertext
-        plaintext = [plaintext[i : i + BLOCK_SIZE * BYTES_PER_WORD] for i in range(self.num_blocks)]
-        ciphertext = plaintext
-        for i in range(self.num_blocks):
-            cipher_output = self.aes.cipher(self.counters[i])
-            for j in range(BLOCK_SIZE * BYTES_PER_WORD):
-                # is this technically incorrect bc not indexing in column major order?
-                ciphertext[i][j] = plaintext[i][j] + cipher_output[j]
-        # return nonce and flattened ciphertext
-        return self.nonce, [c for block in ciphertext for c in block]
+    # encrypt
+    plaintext = [plaintext[i * (BLOCK_SIZE*BYTES_PER_WORD) : (i+1) * (BLOCK_SIZE*BYTES_PER_WORD)] for i in range(num_blocks)]
+    ciphertext = plaintext
+    for i in range(num_blocks):
+        cipher_output = aes.cipher(nonce + counters[i])
+        # print_ln("[Encrypt] cipher_output in block %s = %s", i, [x.reveal() for x in cipher_output])
+        # print_ln("[Encrypt] plaintext in block %s = %s", i, [x.reveal() for x in plaintext[i]])
+        # print_ln("[Encrypt] ciphertext before XOR in block %s = %s", i, [x.reveal() for x in ciphertext[i]])
+        for j in range(BLOCK_SIZE * BYTES_PER_WORD):
+            ciphertext[i][j] = plaintext[i][j] + cipher_output[j]
+        # print_ln("[Encrypt] ciphertext after XOR in block %s = %s", i, [x.reveal() for x in ciphertext[i]])
+    return nonce, [c for block in ciphertext for c in block]
     
-    def decrypt(self, ciphertext: list[sgf2n], nonce) -> list[sgf2n]:
-        '''
-        '''
-        pass
+def aes_ctr_decrypt(key: list[sgf2n], ciphertext: list[sgf2n], nonce: list[cgf2n]) -> list[sgf2n]:
+    '''
+    Decrypt ciphertext with nonce and key using AES-CTR mode. 
+    '''
+    # validate ciphertext length and nonce length and set up counters based on ciphertext length
+    assert(len(ciphertext) % (BLOCK_SIZE * BYTES_PER_WORD) == 0) # Too lazy to deal with padding.
+    assert(len(nonce) == 12) 
+    num_blocks = int(len(ciphertext) / (BLOCK_SIZE * BYTES_PER_WORD))
+    assert(num_blocks <= MAX_BLOCKS)
+    counters = [[cgf2n(x) for x in i.to_bytes(length=4)] for i in range(num_blocks)]
+
+    # AES block cipher setup (performs key expansion)
+    num_rounds_from_key_length = {16: 10, 24: 12, 32: 14}
+    assert(len(key) in num_rounds_from_key_length)  # key must be 16, 24, or 32 bytes long
+    aes = AESCipher(num_rounds_from_key_length[len(key)], key)
+
+    # decrypt
+    ciphertext = [ciphertext[i * (BLOCK_SIZE*BYTES_PER_WORD) : (i+1) * (BLOCK_SIZE*BYTES_PER_WORD)] for i in range(num_blocks)]
+    plaintext = ciphertext
+    for i in range(num_blocks):
+        cipher_output = aes.cipher(nonce + counters[i])
+        # print_ln("[Decrypt] cipher_output in block %s = %s", i, [x.reveal() for x in cipher_output])
+        # print_ln("[Decrypt] ciphertext in block %s = %s", i, [x.reveal() for x in ciphertext[i]])
+        # print_ln("[Decrypt] plaintext before XOR in block %s = %s", i, [x.reveal() for x in plaintext[i]])
+        for j in range(BLOCK_SIZE * BYTES_PER_WORD):
+            plaintext[i][j] = ciphertext[i][j] + cipher_output[j]
+        # print_ln("[Decrypt] plaintext after XOR in block %s = %s", i, [x.reveal() for x in plaintext[i]])
+    return [b for block in plaintext for b in block]
 
 
 if __name__ == "__main__":
@@ -64,16 +82,14 @@ if __name__ == "__main__":
     def test_aes_ctr():
         key_raw = "2b7e151628aed2a6abf7158809cf4f3c"
         key = [sgf2n(x) for x in str_to_hex(key_raw)]
-        aes_ctr = AES_CTR(key, num_blocks=4)
         msg_raw = "68656C6C6F2074686572652C207468697320697320612074657374206D65737361676520666F7220656E6372797074696F6E20707572706F7365732E2E2E2E2E"
         msg = [sgf2n(x) for x in str_to_hex(msg_raw)]
-        nonce, ct = aes_ctr.encrypt(msg)
-        ct = [c.reveal() for c in ct]
-        print_ln("nonce=%s\n ct=%s", nonce, ct)
-
-
-        
-        
+        # test_nonce = [cgf2n(x) for x in [0xb2, 0xf, 0x14, 0xbd, 0x91, 0x25, 0xd8, 0x48, 0xa7, 0xa7, 0x30, 0x1a]]
+        nonce, ct = aes_ctr_encrypt(key, msg)
+        pt = aes_ctr_decrypt(key, ct, nonce)
+        print_ln("msg=%s", [x.reveal() for x in msg])
+        print_ln("final plaintext = %s", [x.reveal() for x in pt])
+        print_ln("error pattern = %s", [x.reveal() + y for x,y in zip(msg, pt)])
 
         
 
