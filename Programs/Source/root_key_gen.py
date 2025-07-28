402e6d3878df481a25bfcce0688e2fb9ed8c569b
Ethan Lotan <elotan@g.hmc.edu>
2025-07-28 14:00:25 -0700

    Added sgf2n and cgf2n compatibility to shamir.py and root_key_gen.py

--- Programs/Source/root_key_gen.py
+++ Programs/Source/root_key_gen.py
@@ -5,11 +5,12 @@ import os, sys
 sys.path.insert(0, os.path.dirname(sys.argv[0]) + '/../..') 
 
 from Compiler.library import print_ln, listen_for_clients, accept_client_connection, for_range, get_player_id, if_
-from Compiler.types import sint, cint, regint, Array, ClientMessageType
+from Compiler.types import sint, cint, regint, Array, ClientMessageType, sgf2n
 from Compiler.compilerLib import Compiler
 
 # we assume these modules reside in Programs/Source/ 
 from shamir import shamir_share
+from aes import apply_field_embedding
 
 usage = "usage: %prog [options] [args]"
 compiler = Compiler(usage=usage)
@@ -40,18 +41,37 @@ def root_key_gen():
     socket = accept_client_connection(PORT_BASE)
 
     # generate and secret share key
-    key = sint.get_random_int(bits=key_len)
-    # key = sint(val=1) # debugging
-    eval_points, poly_evals = shamir_share(msg=key, threshold=t, num_parties=n)
-    eval_points = eval_points.reveal() # nothing secret about these as all. Should consider changing impl of shamir to make them public. 
+    key = [
+            apply_field_embedding(
+                sgf2n.bit_compose([sgf2n.get_random_bit() for _ in range(8)])
+            ) 
+            for _ in range(key_len//8)
+          ]
 
+    # key = sint.get_random_int(bits=key_len)
+
+    # key = sint(val=1) # debugging
+    eval_list = [
+            apply_field_embedding(sgf2n(i)) for i in range(n)]
+
+    key_eval_points = Array(n,sgf2n).assign(eval_list)
+
+    rand_list = [
+        apply_field_embedding(sgf2n.bit_compose([sgf2n.get_random_bit() for _ in range(8)])) for i in range(t)]
+    
+    key_rand = Array(t,sgf2n).assign(rand_list)
+
+    poly_evals = []
+    for i in range(key_len//8):
+        poly_evals.append(shamir_share(msg=key[i], threshold=t, num_parties=n, eval_points=key_eval_points, rand=key_rand)[1])
+   
     # write Shamir shares of key back to client. 
-    @for_range(regint(n))
-    def _(i):
-        poly_eval_personal = poly_evals[i].reveal_to(i)
-        @if_(i == socket) # think this is equiv to @if_(i == regint(get_player_id()._v)) 
+    for i in range(n):
+        @if_(regint(i) == socket) # think this is equiv to @if_(i == regint(get_player_id()._v)) 
         def _():
-            cint.write_to_socket(socket, [eval_points[i], poly_eval_personal._v])
+            for j in range(key_len//8):
+                poly_eval_personal = poly_evals[j][i].reveal_to(regint(i))
+                cint.write_to_socket(socket, poly_eval_personal._v)
 
     # print_ln("KEY=%s\n", key.reveal()) # debugging
 
--- Programs/Source/shamir.py
+++ Programs/Source/shamir.py
@@ -4,13 +4,13 @@ import os, sys
 # add MP-SPDZ dir to path so we can import from Compiler
 sys.path.insert(0, os.path.dirname(sys.argv[0]) + '/../..') 
 from Compiler.library import print_ln
-from Compiler.types import sint, cint, Array
+from Compiler.types import sint, cint, Array, sgf2n, cgf2n
 from Compiler.compilerLib import Compiler # only used for testing
 
 # we assume these modules reside in Programs/Source/ 
 from linalg import LUSolver, create_vandermonde_matrix
 
-def shamir_share(msg, threshold, num_parties, eval_points=None, rand=None):
+def shamir_share(msg: sint|sgf2n, threshold: int, num_parties: int, eval_points:Array=None, rand:Array=None) -> tuple[Array, Array]:
     '''
     Perform textbook Shamir's secret sharing
 
@@ -27,15 +27,21 @@ def shamir_share(msg, threshold, num_parties, eval_points=None, rand=None):
     share of the form (x, p(x))
     '''
     assert threshold <= num_parties
+    msg_type = type(msg)
+    print(f"msg_type={msg_type}")
     if eval_points is None:
-        eval_points = Array(num_parties, sint).assign([i for i in range(1,num_parties+1)]) # TODO: do we need sint if we return eval_points as part of tuple?
+        eval_points = Array(num_parties, msg_type).assign([i for i in range(1,num_parties+1)]) # TODO: do we need sint if we return eval_points as part of tuple?
     
-    V = create_vandermonde_matrix(num_parties, threshold - 1, sint, eval_points)
-    poly_coeffs = Array(threshold, sint)
+    V = create_vandermonde_matrix(num_parties, threshold - 1, msg_type, eval_points)
+    poly_coeffs = Array(threshold, msg_type)
     if rand:
         poly_coeffs.assign(rand)
     else:
-        poly_coeffs.randomize()
+        if msg_type == sgf2n:
+            for i in range(len(poly_coeffs)):
+                poly_coeffs[i] = sgf2n.bit_compose([sgf2n.get_random_bit() for _ in range(128)])
+        else:
+            poly_coeffs.randomize()
     poly_coeffs[0] = msg
     poly_evals = V.dot(poly_coeffs)
     # shares = Matrix(2, num_parties, sint)
@@ -52,7 +58,7 @@ def shamir_reconstruct(eval_points, poly_evals):
     In other words, (eval_points[i], poly_evals[i]) is a single Shamir secret share of the form (x, p(x))
     :returns: A reconstructed secret, interpreted as a field element in the computation domain
     '''
-    V = create_vandermonde_matrix(eval_points.length, eval_points.length - 1, cint)
+    V = create_vandermonde_matrix(eval_points.length, eval_points.length - 1, type(poly_evals[0]))
     solver = LUSolver(V)
     poly_coeffs = solver.solve(poly_evals)
     return poly_coeffs[0]
@@ -66,12 +72,30 @@ if __name__ == "__main__":
         print_ln("SHAMIR TESTS")
 
         print_ln("----Test 1----")
-        msg = sint(12)
+        msg = sint(246432)
         rand = sint.Array(2).assign([4, 5])
         print_ln("msg=%s", msg.reveal())
         eval_points, poly_evals = shamir_share(msg, 2, 3, rand=rand)
         print_ln("eval_points=%s \npoly_evals=%s", eval_points.reveal(), poly_evals.reveal())
         reconstructed_msg = shamir_reconstruct(eval_points, poly_evals)
         print_ln("reconstructed_msg=%s", reconstructed_msg.reveal())
+
+        print_ln("----Test 2 (sgf2n with a random array)----")
+        msg = sgf2n(700)
+        rand = Array(2, sgf2n).assign([88, 57])
+        print_ln("msg=%s", msg.reveal())
+        eval_points, poly_evals = shamir_share(msg, 2, 3, rand=rand)
+        print_ln("eval_points=%s \npoly_evals=%s", eval_points.reveal(), poly_evals.reveal())
+        reconstructed_msg = shamir_reconstruct(eval_points, poly_evals)
+        print_ln("reconstructed_msg=%s", sgf2n(reconstructed_msg.reveal()).reveal())
+
+
+        print_ln("----Test 3 (sgf2n)----")
+        msg = sgf2n(1283890184043)
+        print_ln("msg=%s", msg.reveal())
+        eval_points, poly_evals = shamir_share(msg, 2, 3)
+        print_ln("eval_points=%s \npoly_evals=%s", eval_points.reveal(), poly_evals.reveal())
+        reconstructed_msg = shamir_reconstruct(eval_points, poly_evals)
+        print_ln("reconstructed_msg=%s", sgf2n(reconstructed_msg.reveal()).reveal())
 
     compiler.compile_func()  
