dcf72663dbc3523189852d83a1fc7fea919b0f95
Christopher Smith <csasmith@cs.stonybrook.edu>
2026-07-20 14:48:07 -0400

    progress on sv lrss

--- Programs/Source/lrss.py
+++ Programs/Source/lrss.py
@@ -11,90 +11,127 @@ from new_shamir import shamir_share, shamir_reconstruct
 from utils import dot_product_random_preimage, apply_field_embedding, apply_inverse_field_embedding, get_random_sgf2n
 
 def get_source_length(
-    leakage_budget: int, 
-    leakage_secpar: int, 
-    field_bit_length: int, 
-    num_parties: int
+    num_parties: int,
+    mu: int,
+    secpar: int, 
+    k: int=128, 
 ) -> int:
     '''
     Helper function for determining (at compile-time) the source length for the
-    inner product extractor used in the CKOS LRSS
+    inner product extractor used in the SV LRSS scheme. See 3.2 in LRPSS paper
+    for derivation.
     '''
-    # see CKOS Theorem 1 for ext_error
-    # see leftover hash lemma for min_entropy
-    # for source_length, observe source needs (min_entropy + leakage_budget) total
-    # min entropy before leakage. Add 1 because of how we sample source. Divide
-    # by field_bit_length to get source_length in terms of embedded field elements
-    ext_error = (2 ** (- leakage_secpar)) / (6 * num_parties)
-    min_entropy = field_bit_length + 2 * math.log2(1 / ext_error) - 2
-    source_length = math.ceil( (min_entropy + leakage_budget + 1) / field_bit_length)
+    eps_ext = (2 ** (- secpar)) / (2 * num_parties)
+    lower_bound = k + mu + 2 * math.log2(1 / eps_ext) - 2
+    source_length = math.ceil(lower_bound / k) # eta / k
     return source_length
 
 
 
-def lr_share[T: sgf2n](
-        msg: T,
+def lr_share(
+        msg: sgf2n,
         threshold: int,
         num_parties: int,
-        leakage_budget: int, 
-        leakage_secpar: int=40, 
-        field_bit_length: int=8,
+        mu: int, 
+        secpar: int=40, 
+        field_bit_length: int=128,
         size: int=1,
-        embedded: bool=True
-    ) -> tuple[list, list, list]:
+    ) -> list[list]:
     '''
-    LRShare algorithm of CKOS22 leakage-resilient secret sharing scheme.
-    https://eprint.iacr.org/2022/216
-    We hardcode Shamir's secret sharing for MShare and SdShare, and instantiate
-    the strong linear extractor with the inner product extractor.
-    The consequence is that the resulting LRSS scheme enjoys perfect privacy and perfect local uniformity, 
-    and we can easily set the leakage error with a single parameter leakage_secpar.
+    LRShare algorithm of Srinivasan and Vasudevan strong local leakage-resilient
+    secret sharing scheme. https://eprint.iacr.org/2018/1154.
     
-    :param msg: message to be shared. If msg is embedded, set embedded flag.
+    We hardcode Shamir's secret sharing with default eval points
+    1,...,num_parties for the underlying threshold scheme, and instantiate the
+    extractor with the inner product extractor. The source/seed length is
+    determined by a desired statistical security parameter **secpar**, and a
+    desired leakage budget **mu**.  
+    
+    :param msg: message to be shared.
     :param threshold: secret sharing threshold
-    :param num_parties: number of shares
-    :param leakage_budget: number of bits of local leakage allowed on a share
-    :param leakage_secpar: 2^{- leakage_secpar} is desired statistical distance
-    between leakage distributions on share sets of any two messages (see paper).
-    :param field_bit_length: number of bits needed to represent an element of
-    the field in which the message lives (check embedding flag)
-    :param size: the usual MP-SPDZ parallelization parameter
-    :param embedded: is msg an embedded field element. For now we only support
-    embedded elements so this is always set to true. 
+    :param num_parties: number of shareholders
+    :param mu: number of bits of local leakage allowed on a share
+    :param secpar: 2**(- secpar) is desired statistical distance
+    between views on share sets of any two messages (see paper). 
+    :param field_bit_length: bit-length of field msg lives in. Only 128 for now. 
+    :param size: the usual MP-SPDZ parallelization argument 
     '''
-    # handles for message type and corresponding clear type
-    t = type(msg)
-    ct = t if not hasattr(t, "clear_type") else t.clear_type
-
-    # determine appropriate seed length (= source length)
-    source_length = get_source_length(leakage_budget, leakage_secpar, field_bit_length, num_parties)
+    source_length = get_source_length(num_parties, mu, secpar)
     seed_length = source_length
 
     # Shamir share msg to get intermediate shares
-    eval_points = [apply_field_embedding(ct(i, size=size)) for i in range(1,num_parties+1)]
-    rand = [apply_field_embedding(get_random_sgf2n(field_bit_length, size=size)) for _ in range(threshold)]
-    _, intermediate_shares = shamir_share(msg, threshold, num_parties, rand=rand, size=size)
+    eval_points = [cgf2n(i, size=size) for i in range(1,num_parties+1)]
+    intermediate_shares = shamir_share(msg, threshold, num_parties, size=size)[1]
+
+    # uniformly sample one extractor seed, num_parties sources, and num_parties masks
+    seed = [get_random_sgf2n(field_bit_length, size=size) for _ in range(seed_length)]
+    sources = [[get_random_sgf2n(field_bit_length, size=size) for _ in range(source_length)] for _ in range(num_parties)]
+    masks = [get_random_sgf2n(field_bit_length, size=size) for _ in range(num_parties)]
+
+    # double mask intermediate shares
+    # crucially assumes characteristic-two field where addition is XOR
+    ext_outputs = [[sum(seed[j] * source[j]) for j in range(seed_length)] for source in sources]
+    ct = [intermediate_shares[i] + ext_outputs[i] + masks[i] for i in range(num_parties)]
     
-    # sample uniform extractor seed
-    seed = [apply_field_embedding(get_random_sgf2n(field_bit_length, size=size)) for _ in range(seed_length)]
-    # (2,n) secret share the extractor seed
-    rands = [[apply_field_embedding(get_random_sgf2n(field_bit_length, size=size)) for _ in range(2)] for _ in range(seed_length)]
-    seed_shares = [shamir_share(seed[i], 2, num_parties, eval_points=eval_points, rand=rands[i], size=size)[1] for i in range(seed_length)]
-    seed_shares_by_party = {party: [] for party in range(num_parties)}
-    for i in range(seed_length):
-        seed_shares = shamir_share(
-            msg=seed[i],
+    # share masks and seed.
+    mask_shares = [shamir_share(masks[i], threshold, num_parties, size=size)[1] for i in range(num_parties)]
+    mask_shares_transposed = list(map(list, zip(*mask_shares)))
+    seed_shares_transposed = [shamir_share(seed[j], threshold, num_parties, size=size)[1] for j in range(seed_length)]
+    seed_shares = list(map(list, zip(*seed_shares_transposed)))
+
+    # output final shares
+    shares = [[sources[i], ct[i], seed_shares[i], mask_shares_transposed[i]] for i in range(num_parties)]
+    return shares
+
+def lr_rec(
+        shares: list[list],
+        coords: list[cgf2n]=None,
+        size: int=1
+    ):
+    '''
+    LRRec algorithm of Srinivasan and Vasudevan strong local leakage-resilient
+    secret sharing scheme. https://eprint.iacr.org/2018/1154. 
+
+    :param shares: each list in shares represents an LRSS share of the form
+    output by lr_share. 
+    :param coords: coords[i] indicates which party shares[i] comes from. If
+    None, assumes coords = [cgf2n(i) for i in range(1, len(shares)+1)]
+    :param size: the usual parallelization parameter
+    '''
+    if not coords: coords = [cgf2n(i) for i in range(1, len(shares)+1)]
+    [sources, ct, seed_shares, mask_shares_transposed] = list(map(list, zip(*shares)))
+
+    # reconstruct masks and seed
+    mask_shares = list(map(list, zip(mask_shares_transposed)))
+    masks = [shamir_reconstruct(s, coords, size=size) for s in mask_shares]
+    seed_shares_transposed = list(map(list, zip(*seed_shares)))
+    seed = [shamir_reconstruct(s, coords, size=size) for s in seed_shares_transposed]
+
+    # unmask intermediate shares
+    ext_outputs = [[sum(seed[j] * source[j]) for j in range(len(seed))] for source in sources]
+    
+    return msg
+    
+
+if __name__ == "__main__":
+    usage = "usage: %prog [options] [args]"
+    compiler = Compiler(usage=usage)
+
+    @compiler.register_function("test_lrss")
+    def test_lrss():
+        print_ln("LRSS TESTS")
+
+        print_ln("-----TEST 1-----")
+        msg = sgf2n(2)
+        shares = lr_share(
+            msg=msg,
             threshold=2,
-            num_parties=num_parties,
-            eval_points=eval_points,
-            rand=rands[i],
-            size=size
-        )[1]
-        for party, sh in enumerate(seed_shares):
-            seed_shares_by_party[party].append(sh)
-
-    # sample sources. *** uses ORAM ***
-    sources = [dot_product_random_preimage(seed, intermediate_shares[i]) for i in range(num_parties)]
-
-
+            num_parties=3,
+            mu=1,
+            secpar=40,
+        )
+        rec_msg = lr_rec(*shares)
+        error_pattern = (rec_msg - msg).reveal()
+    
+    compiler.compile_func()
 
